57de1e48c955d775bc37dd0083c0395282787ca8
Fraan-nz <franco.nnz@hotmail.com>
2021-10-26 12:35:35 -0300

    loading spinner

--- src/components/ItemDetail/ItemDetailContainer.js
+++ src/components/ItemDetail/ItemDetailContainer.js
@@ -3,23 +3,33 @@ import { useParams } from "react-router-dom";
 import ItemDetail from "./ItemDetail";
 import { useCart } from "../../context/CartContext";
 import { getDataById } from "../../firebase/index";
+import { Spinner } from "../Spinner/Spinner";
 
 function ItemDetailContainer() {
 	const [product, setProduct] = useState({});
+	const [loading, setLoading] = useState(false);
 	const { cartList } = useCart();
 	const { idProd } = useParams();
 
 	useEffect(() => {
+		setLoading(true);
 		getDataById(idProd)
-			.then((res) => setProduct(res))
+			.then((res) => {
+				setProduct(res);
+				setLoading(false);
+			})
 			.catch((error) => {
 				console.log(error);
 			});
 	}, [idProd]);
 	return (
 		<>
-			{Object.keys(product).length !== 0 && (
-				<ItemDetail product={product} cartList={cartList} />
+			{loading ? (
+				<Spinner />
+			) : (
+				Object.keys(product).length !== 0 && (
+					<ItemDetail product={product} cartList={cartList} />
+				)
 			)}
 		</>
 	);
--- src/components/ItemListContainer/ItemListContainer.js
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import ReactPaginate from "react-paginate";
+import { Spinner } from "../Spinner/Spinner";
 import { useParams } from "react-router-dom";
 import { getData, getDataCategory } from "../../firebase";
 import usePagination from "../Pagination/usePagination";
@@ -9,40 +10,54 @@ import "../Pagination/Pagination.css";
 
 function ItemListContainer() {
 	const [data, setData] = useState([]);
+	const [loading, setLoading] = useState(false);
 	const { category } = useParams();
 	const prodsPerPage = 10;
 	const { jumpPage, currentData, maxPage } = usePagination(data, prodsPerPage);
 	const changePage = ({ selected }) => {
 		jumpPage(selected + 1);
 	};
+
 	useEffect(() => {
+		setLoading(true);
 		category
 			? getDataCategory(category)
-					.then((res) => setData(res))
+					.then((res) => {
+						setData(res);
+						setLoading(false);
+					})
 					.catch((error) => {
 						console.log(error);
 					})
 			: getData()
-					.then((res) => setData(res))
+					.then((res) => {
+						setData(res);
+						setLoading(false);
+					})
 					.catch((error) => {
 						console.log(error);
 					});
 	}, [category]);
 	return (
 		<>
-			<ItemList products={currentData()} />
-			<ReactPaginate
-				previousLabel={"<<"}
-				nextLabel={">>"}
-				pageCount={maxPage}
-				initialPage={1}
-				onPageChange={changePage}
-				containerClassName={"paginate__container"}
-				previousLinkClassName={"paginate__btn"}
-				nextLinkClassName={"paginate__btn"}
-				pageLinkClassName={"paginate__link"}
-				activeLinkClassName={"paginate__active"}
-			/>
+			{loading ? (
+				<Spinner />
+			) : (
+				<>
+					<ItemList products={currentData()} />
+					<ReactPaginate
+						previousLabel={"<<"}
+						nextLabel={">>"}
+						pageCount={maxPage}
+						onPageChange={changePage}
+						containerClassName={"paginate__container"}
+						previousLinkClassName={"paginate__btn"}
+						nextLinkClassName={"paginate__btn"}
+						pageLinkClassName={"paginate__link"}
+						activeLinkClassName={"paginate__active"}
+					/>
+				</>
+			)}
 		</>
 	);
 }
